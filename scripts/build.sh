07c7f28a590a32ff845aaae86155aba8c0845a5a
Samuel Cochran <sj26@sj26.com>
2017-02-16 13:00:40 +1100

    Build with golang plugin

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go build -o pkg/golang-example main.go
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Get build + test dependencies. -d also doesn't bother with installing the
-# packages, it just downloads them
-go get -t -d
 go test
